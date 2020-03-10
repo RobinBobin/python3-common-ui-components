a2bd04af7c59dda8f162414011e6764c0d0d0173
Ruben Shalimov <r_shalimov@inbox.ru>
2020-03-10 21:30:00 +0300

    0.0.1.

---
+++ commonuicomponents/labeledscale.py
@@ -0,0 +1,36 @@
+from math import floor, log10
+from tkinter import E, Frame, IntVar, W
+from tkinter.ttk import Label, Scale, Style
+
+class LabeledScale(Frame):
+   def __init__(self, master, caption, from_, to):
+      super().__init__(master, background = "cyan")
+      
+      style = Style()
+      
+      CAPTION = "TLabeledScale.Caption.TLabel"
+      
+      print(style.lookup(CAPTION, "padx", default = "20 10"))
+      
+      Label(self, text = caption).grid(row = 0, column = 0, padx = (20, 10), sticky = E)
+      
+      self.__value = Label(self, anchor = E, text = from_, width = floor(log10(to)) + 1)
+      self.__value.grid(row = 0, column = 1, sticky = E)
+      
+      self.__variable = IntVar()
+      self.__variable.set(from_)
+      
+      Scale(
+         self,
+         command = self.onCommand,
+         from_ = from_,
+         length = 400,
+         to = to,
+         variable = self.__variable).grid(
+            row = 0,
+            column = 2,
+            padx = 20,
+            pady = 20)
+   
+   def onCommand(self, _ = "dummy"):
+      self.__value.configure(text = self.__variable.get())
---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+   long_description = fh.read()
+
+setuptools.setup(
+   name="common-ui-components",
+   version="0.0.1",
+   author="Ruben Shalimov",
+   author_email="r_shalimov@inbox.ru",
+   description="tkinter.ttk-based UI components",
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   url="https://github.com/RobinBobin/python3-common-ui-components",
+   packages=setuptools.find_packages(),
+   classifiers=[
+      "Programming Language :: Python :: 3",
+      "Operating System :: OS Independent"
+   ]
+)
