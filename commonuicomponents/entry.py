d5e88bd30c8459cd81384d3eb125ef1f6046c970
Ruben Shalimov <r_shalimov@inbox.ru>
2020-04-17 00:07:08 +0300

    1) 0.7.1. 2) Entry - default text is kw["text"].

--- commonuicomponents/entry.py
+++ commonuicomponents/entry.py
@@ -10,12 +10,15 @@ class Entry(SmartWidget, TtkEntry):
       if "font" not in kw and Entry.__FONT:
          kw["font"] = Entry.__FONT
       
+      value = StringVar()
+      value.set(kw.get("text", ""))
+      
       kw["hasValueBuffer"] = True
-      kw["value"] = kw["textvariable"] = StringVar()
+      kw["value"] = kw["textvariable"] = value
       
       SmartWidget.__init__(self, master, **kw)
       
-      self.value.set(StaticUtils.getOrSetIfAbsent(self._smartWidgetValueBuffer, 0, ""))
+      self.value.set(StaticUtils.getOrSetIfAbsent(self._smartWidgetValueBuffer, 0, self.value.get()))
       
       self.value.trace_add("write", lambda *_: StaticUtils.setSafely(self._smartWidgetValueBuffer, 0, self.value.get()))
    
