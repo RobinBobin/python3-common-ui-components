b19b716f13a379c765863f35fa0a2438dfecc179
Ruben Shalimov <r_shalimov@inbox.ru>
2020-04-22 01:19:57 +0300

    1) 0.13.5. 2) Checkbutton has no value buffer by default.

--- commonuicomponents/ttk/checkbutton.py
+++ commonuicomponents/ttk/checkbutton.py
@@ -3,8 +3,9 @@ from .smartwidget import SmartWidget
 
 class Checkbutton(SmartWidget, TtkCheckbutton):
    def __init__(self, master = None, **kw):
-      SmartWidget._setVariable(kw, "BooleanVar", variableKey = "variable")
+      SmartWidget._setVariable(kw, "BooleanVar", setHasValueBuffer = False, variableKey = "variable")
       
       SmartWidget.__init__(self, master, **kw)
       
-      self._initValueAndTraceAdd()
+      if self.hasValueBuffer:
+         self._initValueAndTraceAdd()
