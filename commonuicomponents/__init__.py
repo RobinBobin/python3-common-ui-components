10beaf5ae66371f74325a6cbf7074906440b89db
Ruben Shalimov <r_shalimov@inbox.ru>
2020-07-24 19:49:52 +0300

    1) 2.4.0. 2) commonuicomponents.periodicalafter.PeriodicalAfter added.

--- commonuicomponents/__init__.py
+++ commonuicomponents/__init__.py
@@ -3,5 +3,6 @@ from .applauncher import AppLauncher
 from .basetab import BaseTab
 from .basetabloader import BaseTabLoader
 from .json import Config, Storage
+from .periodicalafter import PeriodicalAfter
 from .staticutils import StaticUtils
 from .version import __version__
---
+++ commonuicomponents/periodicalafter.py
@@ -0,0 +1,25 @@
+class PeriodicalAfter:
+   def __init__(self, widget, callback, *args):
+      self.__args = tuple(args)
+      self.__callback = callback
+      self.__id = None
+      self.__widget = widget
+   
+   def start(self, delay_ms = 0):
+      if self.__id:
+         raise RuntimeError()
+      
+      def callback():
+         self.__callback(*self.__args)
+         
+         setId()
+      
+      def setId():
+         self.__id = self.__widget.after(delay_ms, callback) if delay_ms else self.__widget.after_idle(callback)
+      
+      setId()
+   
+   def stop(self):
+      self.__widget.after_cancel(self.__id)
+      
+      self.__id = None
