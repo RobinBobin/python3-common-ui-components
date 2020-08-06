17da341ca4083a4d1d2e3e87d1222c83b5dfe5c7
Ruben Shalimov <r_shalimov@inbox.ru>
2020-08-06 15:02:29 +0300

    1) 2.18.1. 2) commonuicomponents.ttk.widgets.statefulbutton.StatefulButton can be made serializable.

--- commonuicomponents/ttk/widgets/statefulbutton.py
+++ commonuicomponents/ttk/widgets/statefulbutton.py
@@ -1,19 +1,24 @@
+from commonutils import DictPopper
 from tkinter.ttk import Button
 from .smartwidget import SmartWidget
 
 class StatefulButton(SmartWidget, Button):
    def __init__(self, master, **kw):
-      self.__states = kw.pop("states")
+      hasValueBuffer, self.__states = DictPopper(kw).add("hasValueBuffer", False).add("states")
       
       SmartWidget._setVariable(kw, "IntVar")
       
       SmartWidget.__init__(self, master, **kw)
       
-      self.__setText()
+      if hasValueBuffer:
+         self._initValueAndTraceAdd()
+      
+      self.traceWrite(self.onStateChanged)
+      
+      self.onStateChanged()
    
    def nextState(self):
       self.getRawValue().set((self.getValue() + 1) % len(self.__states))
-      self.__setText()
    
-   def __setText(self):
+   def onStateChanged(self):
       self["text"] = self.__states[self.getValue()]
